dea8c2eb17ed7744964643ce90f5b78db20c39e7
Maks <makzylinski@gmail.com>
2020-05-10 13:14:27 +0200

    basic rwd done

--- die-hard-zine/src/app/shared/navbar/navbar.component.ts
+++ die-hard-zine/src/app/shared/navbar/navbar.component.ts
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent {
   
   hamVisible = false;
+  subpages: Array<{name: string, path: string}> = [
+    { name: 'Newsy', path: '/news' },
+    { name: 'Recenzje', path: '/reviews' },
+    { name: 'Relacje', path: '/event-reports' },
+    { name: 'Patronaty', path: '/patronage' },
+    { name: 'Wywiady', path: '/interview' },
+    { name: 'O nas', path: '/about' },
+    { name: 'Kontakt', path: '/contact' }
+  ];
 
   constructor() { }
 
